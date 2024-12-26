3325d1b7b9021ab4fc92373922d385c1b8b4cbbc
Tanmay Singhal <tanmay22535@iiitd.ac.in>
2024-12-26 15:52:37 +0530

    changes

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,15 +1,33 @@
+const jwt = require("jsonwebtoken");
 const blogsRouter = require("express").Router();
+const User = require("../models/user");
 const Blog = require("../models/blog");
+const user = require("../models/user");
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "");
+  }
+  return null;
+};
 
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
-  response.json(blogs);
+  const notes = await Blog.find({}).populate("user", { username: 1, name: 1 });
+  response.json(notes);
 });
 
 blogsRouter.post("/", async (request, response) => {
-  const blog = new Blog(request.body);
-  const result = await blog.save();
-  response.status(201).json(result);
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "token invalid" });
+  }
+  const user = await User.findById(decodedToken.id);
+  const blog = new Blog({ ...request.body, user: user });
+  const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog);
+  await user.save();
+  response.status(201).json(savedBlog);
 });
 
 blogsRouter.delete("/:id", async (request, response) => {
---
+++ part4/blog/controllers/users.js
@@ -0,0 +1,26 @@
+const bcrypt = require("bcrypt");
+const usersRouter = require("express").Router();
+const User = require("../models/user");
+
+usersRouter.get("/", async (request, response) => {
+  const users = await User.find({}).populate("blogs", {
+    url: 1,
+    title: 1,
+    author: 1,
+  });
+  response.json(users);
+});
+
+usersRouter.post("/", async (request, response) => {
+  const body = request.body;
+  const passwordHash = await bcrypt.hash(body.password, 10);
+  const user = new User({
+    username: body.username,
+    name: body.name,
+    passwordHash: passwordHash,
+  });
+  const result = await user.save();
+  response.status(201).json(result);
+});
+
+module.exports = usersRouter;
